2e5cd0179aac39511c98f3178a80f3463d8087ea
917_Timofte_Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2023-11-18 13:06:04 +0200

    changed ids to numeric type for automatic indexing purposes

--- AdoptMe.Common/Models/Animal.cs
+++ AdoptMe.Common/Models/Animal.cs
@@ -2,15 +2,15 @@
 {
     public class Animal
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Gender { get; set; }
         public DateTime DateOfBirth {  get; set; }
         public string Color { get; set; }
         public string Description { get; set; }
-        public string ShelterId {  get; set; }
+        public int ShelterId {  get; set; }
 
-        public Animal(string id, string name, string gender, DateTime dateOfBirth, string color, string description, string shelterId)
+        public Animal(int id, string name, string gender, DateTime dateOfBirth, string color, string description, int shelterId)
         {
             Id = id;
             Name = name;
@@ -21,7 +21,7 @@
             ShelterId = shelterId;
         }
 
-        public Animal(string name, string gender, DateTime dateOfBirth, string color, string description, string shelterId)
+        public Animal(string name, string gender, DateTime dateOfBirth, string color, string description, int shelterId)
         {
             Name = name;
             Gender = gender;
