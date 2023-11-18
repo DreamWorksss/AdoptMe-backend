bb92ef628164021b8181d08c9a5239c2770067c3
917_Timofte_Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2023-11-18 19:44:44 +0200

    Added generic service class

---
+++ AdoptMe.Service/GenericService.cs
@@ -0,0 +1,41 @@
+﻿using AdoptMe.Repository;
+using AdoptMe.Repository.DataContext;
+using AdoptMe.Service.Interfaces;
+
+namespace AdoptMe.Service
+{
+    public class GenericService<T> : IGenericService<T> where T : class
+    {
+        private readonly GenericRepository<T> _repository;
+
+        public GenericService(GenericRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public void Add(T entity)
+        {
+            _repository.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _repository.Delete(entity);
+        }
+
+        public List<T> RetrieveAll()
+        {
+            return _repository.RetrieveAll();
+        }
+
+        public T? RetrieveById(int entityId)
+        {
+            return _repository.RetrieveById(entityId);
+        }
+
+        public void Update(T entity)
+        {
+            _repository.Update(entity);
+        }
+    }
+}
---
+++ AdoptMe.Service/Interfaces/IGenericService.cs
@@ -0,0 +1,32 @@
+﻿namespace AdoptMe.Service.Interfaces
+{
+    interface IGenericService<T> where T : class
+    {
+        /// <summary>
+        /// Retrieves specific entity by id
+        /// </summary>
+        /// <param name="entityId"></param>
+        /// <returns>The entity if found, null otherwise</returns>
+        T? RetrieveById(int entityId);
+        /// <summary>
+        /// Retrieves a list of all entities
+        /// </summary>
+        /// <returns>The list of all entities</returns>
+        List<T> RetrieveAll();
+        /// <summary>
+        /// Adds the entity to the database
+        /// </summary>
+        /// <param name="entity"></param>
+        void Add(T entity);
+        /// <summary>
+        /// Updates the existing entity
+        /// </summary>
+        /// <param name="entity"></param>
+        void Update(T entity);
+        /// <summary>
+        /// Deletes the existing entity from the database
+        /// </summary>
+        /// <param name="entity"></param>
+        void Delete(T entity);
+    }
+}
