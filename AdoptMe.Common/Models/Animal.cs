6560092fa3f39d97b58caf28d06257278a9072e5
917_Timofte_Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2023-11-18 12:28:42 +0200

    created basic animal class

--- AdoptMe.Common/Models/Animal.cs
+++ AdoptMe.Common/Models/Animal.cs
@@ -1,6 +1,41 @@
-﻿namespace AdoptMe.Common.Models
+﻿using System.Drawing;
+
+namespace AdoptMe.Common.Models
 {
     public class Animal
     {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth {  get; set; }
+        public string Color { get; set; }
+        public string Description { get; set; }
+        public string ShelterId {  get; set; }
+
+        public Animal()
+        {
+            
+        }
+
+        public Animal(string id, string name, string gender, DateTime dateOfBirth, string color, string description, string shelterId)
+        {
+            Id = id;
+            Name = name;
+            Gender = gender;
+            DateOfBirth = dateOfBirth;
+            Color = color;
+            Description = description;
+            ShelterId = shelterId;
+        }
+
+        public Animal(string name, string gender, DateTime dateOfBirth, string color, string description, string shelterId)
+        {
+            Name = name;
+            Gender = gender;
+            DateOfBirth = dateOfBirth;
+            Color = color;
+            Description = description;
+            ShelterId = shelterId;
+        }
     }
 }
