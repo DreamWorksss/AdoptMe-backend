73b4156daa840fc3994e5b39aca9884bb1ecfe23
917_Timofte_Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2023-12-28 19:15:17 +0200

    created Shelter model (image storage pending)

---
+++ AdoptMe.Repository/Models/Shelter.cs
@@ -0,0 +1,28 @@
+﻿using System.Buffers.Text;
+using System.Drawing;
+
+namespace AdoptMe.Repository.Models
+{
+    public class Shelter
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        public Shelter(int id, string name, string description, string image)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Image = image;
+        }
+
+        public Shelter(string name, string description, string image)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+        }
+    }
+}
